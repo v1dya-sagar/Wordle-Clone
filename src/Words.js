2503c23b98781cec5c7f4d21ef9227f25e2e3f24
v1dya-sagar <vidyasagar08123@gmail.com>
2024-05-22 07:56:58 +0530

    Initial Commit

---
+++ src/Words.js
@@ -0,0 +1,24 @@
+import wordBank from "./wordle-bank.txt";
+
+export const boardDefault = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+
+export const generateWordSet = async () => {
+  let wordSet;
+  let todaysWord;
+  await fetch(wordBank)
+    .then((response) => response.text())
+    .then((result) => {
+      const wordArr = result.split("\n");
+      todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)];
+      console.log(todaysWord);
+      wordSet = new Set(wordArr);
+    });
+  return { wordSet, todaysWord };
+};
